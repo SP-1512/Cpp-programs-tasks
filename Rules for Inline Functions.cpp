dc0e2b66e7b4f6c9cc6d69346ce9a1049414ce8f
Sriram Pinninti <pinnintisriram2006@gmail.com>
2025-08-08 10:34:05 +0530

    Update Rules for Inline Functions.cpp

--- Rules for Inline Functions.cpp
+++ Rules for Inline Functions.cpp
@@ -1,36 +1,52 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
-inline void Add(int x, int y) {
-cout << x + y << endl;
-}
-inline void Sub(int x, int y ) {
-cout << x - y << endl;
-}
-inline void Multi(int x, int y ) {
-cout << x * y << endl;
-}
-int main (){
-	int n1,n2;
-	ch n;
-	cout << "Enter the two numbers : " << endl;
-	cin >> n1 >> n2;
-	
-	cout << "Enter the sign of the operation to perform : "
-	cin >> n;
-	
-	switch(n){
-		case '+':
-			Add(n1 ,n2);
-			break;
-		case '-':
-			Sub(n1 ,n2);
-			break;
-		case '*':
-			Multi(n1 ,n2);
-			break;
-		default : 
-		cout << "Entered Sign must be + , - , * only!!!!" << endl;
-	}
+class Calculator {
+    public: int n1,
+    n2;
+    char n;
+    Calculator(int n1, int n2, char n) {
+        this -> n1 = n1;
+        this -> n2 = n2;
+        this -> n = n;
+        switch (n) {
+            case '+':
+                Add(n1, n2);
+                break;
+            case '-':
+                Sub(n1, n2);
+                break;
+            case '*':
+                Multi(n1, n2);
+                break;
+            default:
+                cout << "Entered Sign must be + , - , * only!!!!" << endl;
+        }
+    }
+
+
+    inline void Add(int x, int y) {
+        cout <<"Sum : "<< x + y << endl;
+    }
+    inline void Sub(int x, int y) {
+        cout <<"Difference : "<< x - y << endl;
+    }
+    inline void Multi(int x, int y) {
+        cout <<"Product : "<< x * y << endl;
+    }
+};
+
+int main() {
+    int n1, n2;
+    char n;
+    cout << "Enter the two numbers : " << endl;
+    cin >> n1 >> n2;
+
+    cout << "Enter the sign of the operation to perform : ";
+    cin >> n;
+
+    Calculator c1(n1, n2, n);
+
 
 }
